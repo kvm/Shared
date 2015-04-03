a8404fef5ebf4b959534c2c901ced428a160d5a1
Anilchopra22 <anil.chopra2008@gmail.com>
2015-04-04 03:00:34 +0530

    Removing some comments

--- public/Apps/YoutubeDownloader/YoutubeDownloader/Common/Command.cs
+++ public/Apps/YoutubeDownloader/YoutubeDownloader/Common/Command.cs
@@ -1,56 +1,23 @@
-﻿// <copyright file="Command.cs" company="Microsoft Corporation">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-
+﻿
 namespace YoutubeDownloader.Common {
     using System;
     using System.Windows.Input;
 
-    /// <summary>
-    /// A view model command that can be enabled or disabled, and performs a given <see cref="Action"/> when invoked.
-    /// Optionally includes display text that can be used to caption the command when used with controls like app
-    /// bar commands, context menu items, buttons, etc.
-    /// </summary>
     public class Command : BindableBase, ICommand {
-        /// <summary>
-        /// The action to invoke when this command is executed.
-        /// </summary>
         private readonly Action action;
 
-        /// <summary>
-        /// Controls whether or not a call to Execute will actually invoke the Action.
-        /// </summary>
         private bool isEnabled;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Command"/> class.
-        /// </summary>
-        /// <param name="invocationAction">The <see cref="Action"/> to perform when the command is invoked.</param>
-        /// <param name="isEnabled">A Boolean that specifies whether or not the command is initially enabled.</param>
         public Command(Action invocationAction, bool isEnabled = true)
             : base() {
             this.action = invocationAction.CheckNonNull("invocationAction");
             this.isEnabled = isEnabled;
         }
 
-        /// <summary>
-        /// Raised to indicate that a call to the <see cref="Execute"/> method caused the logic associated with this <see cref="Command"/> to execute because <see cref="IsEnabled"/> was <c>true</c>.
-        /// </summary>
         public event EventHandler Executed;
 
-        /// <summary>
-        /// Raised when the value that would be returned by <see cref="CanExecute"/> changes. 
-        /// </summary>
-        /// <remarks>
-        /// This event is provided for compatibility with <see cref="ICommand.CanExecuteChanged"/>. 
-        /// For data binding scenarios, property change notifications from the <see cref="IsEnabled"/> property should be used instead.
-        /// </remarks>
-        /// <seealso cref="IsEnabled"/>
         public event EventHandler CanExecuteChanged;
 
-        /// <summary>
-        /// Gets or sets a value indicating whether this command is currently enabled.
-        /// </summary>
         public bool IsEnabled {
             get {
                 return this.isEnabled;
@@ -66,29 +33,10 @@ namespace YoutubeDownloader.Common {
             }
         }
 
-        /// <summary>
-        /// Indicates whether this <see cref="Command"/> will respond to a call to <see cref="Execute"/> by invoking its associated logic.
-        /// </summary>
-        /// <param name="parameter">The parameter argument is present for compatibility with <see cref="ICommand.CanExecute"/> and is ignored.</param>
-        /// <returns><c>true</c> if a call to <see cref="Execute"/> would result in this <see cref="Command"/> invoking its associated logic.</returns>
-        /// <remarks>
-        /// This method is provided for compatibility with <see cref="ICommand"/>. 
-        /// </remarks>
-        /// <seealso cref="IsEnabled"/>
         public bool CanExecute(object parameter) {
             return this.IsEnabled;
         }
 
-        /// <summary>
-        /// If <see cref="IsEnabled"/> is <c>true</c>, executes the logic associated with this command.
-        /// </summary>
-        /// <param name="parameter">The parameter argument is present for compatibility with <see cref="ICommand.Execute"/> and is ignored.</param>
-        /// <remarks>
-        /// If invocation actually proceeds, the <see cref="Executed"/> event is raised.
-        /// </remarks>
-        /// <seealso cref="Executed"/>
-        /// <seealso cref="CanExecute"/>
-        /// <seealso cref="IsEnabled"/>
         public void Execute(object parameter = null) {
             if (this.isEnabled) {
                 this.action.Invoke();
--- public/Apps/YoutubeDownloader/YoutubeDownloader/ViewModels/PageModelSettings.cs
+++ public/Apps/YoutubeDownloader/YoutubeDownloader/ViewModels/PageModelSettings.cs
@@ -1,7 +1,4 @@
-﻿// <copyright file="PageModelSettings.cs" company="Microsoft Corporation">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-
+﻿
 namespace YoutubeDownloader.ViewModels
 {
     using System;
@@ -10,41 +7,18 @@ namespace YoutubeDownloader.ViewModels
     using System.Linq;
     using YoutubeDownloader.Common;
 
-    /// <summary>
-    /// Base type for serializable settings classes that encapsulate all of the saved state required
-    /// by a top-level view model (PageModel) to reconstruct itself and any contained view models.
-    /// </summary>
     public abstract class PageModelSettings
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PageModelSettings"/> class.
-        /// </summary>
         protected PageModelSettings()
         {
         }
 
-        /// <summary>
-        /// Overridden in derived types to initialize the instance of <see cref="PageModelSettings"/> based on the serialized property values in <paramref name="savedValues"/>.
-        /// </summary>
-        /// <param name="savedValues">A dictionary that maps property names to string-encoded property values that should be used to initialize this <see cref="PageModelSettings"/> instance.</param>
         public abstract void Load(IDictionary<string, string> savedValues);
 
-        /// <summary>
-        /// Overridden in derived types to serialized the property values of this <see cref="PageModelSettings"/> instance as a collection of property-name/string-encoded property-value pairs.
-        /// </summary>
-        /// <returns>The property name/string-encoded property value pairs for the serialized property values of this <see cref="PageModelSettings"/> instance.</returns>
         public abstract IEnumerable<KeyValuePair<string, string>> Save();
 
         #region Serialization helpers
 
-        /// <summary>
-        /// Attempts to retrieve a setting value of type T with the specified name from the given serialized setting values, returning a default value if the setting is not present.
-        /// </summary>
-        /// <typeparam name="T">type the setting is expected to be</typeparam>
-        /// <param name="fromValues">The dictionary of property name/string-encoded property value pairs from which to load the value.</param>
-        /// <param name="settingName">The name with which to look up the value in the dictionary.</param>
-        /// <param name="defaultValue">The default value to return if no property value with the specified name is found in the dictionary.</param>
-        /// <returns>The Boolean setting value found in the dictionary, or <paramref name="defaultValue"/> if no setting with the specified name was found.</returns>
         protected static T LoadValue<T>(IDictionary<string, string> fromValues, string settingName, T defaultValue)
         {
             T result = defaultValue;
@@ -58,13 +32,6 @@ namespace YoutubeDownloader.ViewModels
             return result;
         }
 
-        /// <summary>
-        /// Generates a key-value pair for the specified setting of type T.
-        /// </summary>
-        /// <typeparam name="T">type the setting is expected to be</typeparam>
-        /// <param name="settingName">The setting name the specifies the key.</param>
-        /// <param name="value">The String setting value.</param>
-        /// <returns>A new <see cref="KeyValuePair&lt;string, string&gt;"/> containing the specified setting name and value.</returns>
         protected static KeyValuePair<string, string> SaveValue<T>(string settingName, T value)
         {
             string stringValue = (string)Convert.ChangeType(value, typeof(string), CultureInfo.InvariantCulture);
@@ -74,10 +41,6 @@ namespace YoutubeDownloader.ViewModels
 
         #endregion
 
-        /// <summary>
-        /// This class is a convenience for page models that do not require settings, and can therefore be declared
-        /// as deriving from <see cref="PageModel&lt;PageModelSettings.None&gt;"/>.
-        /// </summary>
         public sealed class None : PageModelSettings
         {
             /// <summary>
@@ -87,21 +50,11 @@ namespace YoutubeDownloader.ViewModels
             {
             }
 
-            /// <summary>
-            /// <see cref="PageModelSettings.None"/> does not support <see cref="Load"/> or <see cref="Save"/> operations.
-            /// Calling this method on an instance of <see cref="PageModelSettings.None"/> will have no effect.
-            /// </summary>
-            /// <param name="savedValues">This parameter is ignored.</param>
             public override void Load(IDictionary<string, string> savedValues)
             {
                 // Nothing to load.
             }
 
-            /// <summary>
-            /// <see cref="PageModelSettings.None"/> does not support <see cref="Load"/> or <see cref="Save"/> operations.
-            /// Calling this method on an instance of <see cref="PageModelSettings.None"/> will return an empty collection.
-            /// </summary>
-            /// <returns>Always returns an empty <see cref="IEnumerable&lt;KeyValuePair&lt;string, string&gt;&gt;"/>.</returns>
             public override IEnumerable<KeyValuePair<string, string>> Save()
             {
                 // Nothing to save.
