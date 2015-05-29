6879857112341ae93ab4e686a5c38a6bc83041f1
Anilchopra22 <anil.chopra2008@gmail.com>
2015-05-30 00:43:30 +0530

    Back button on download history page

--- public/Apps/YoutubeDownloader/YoutubeDownloader/DownloadHistoryPage.xaml.cs
+++ public/Apps/YoutubeDownloader/YoutubeDownloader/DownloadHistoryPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,6 +28,27 @@ namespace YoutubeDownloader
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             this.DataContext = new DownloadHistoryPageModel();
+            HardwareButtons.BackPressed += this.HistoryPage_BackPressed;
+        }
+
+        private void HistoryPage_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            Frame frame = Window.Current.Content as Frame;
+            if (frame == null)
+            {
+                return;
+            }
+
+            if (frame.CanGoBack)
+            {
+                frame.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            HardwareButtons.BackPressed -= this.HistoryPage_BackPressed;
         }
 
     }
